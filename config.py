2e6765d33d51960548762d94c4a3a503ba1bfb0b
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-07 15:01:11 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+GUILD_ID = YOUR_GUILD_ID  # optional
+DOWNLOAD_URL = "https://github.com/luau-lang/luau/releases/latest/download/luau.exe"  # or .tar.gz for Linux
+VM_FILENAME = "luau.exe"
